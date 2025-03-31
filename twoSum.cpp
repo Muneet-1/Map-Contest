bbad38269ef4c3243a34f41db5df65b6618227bf
Muneet-1 <muneetsadhwani13@gmail.com>
2025-03-31 16:24:55 +0530

    Add files via upload

---
+++ twoSum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> sum;
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if(sum.find(complement) != sum.end()) {
+                return {sum(complement), i};
+            }
+            sum(nums[i]) = i
+        }
+    }
+};
